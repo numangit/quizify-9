bc35a5bdc46641eb15e0abb2e132272f586694b7
numangit <abdulnumanjob@gmail.com>
2022-10-10 23:24:31 +0600

    react router setup done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import { RouterProvider } from 'react-router-dom';
+import { router } from './utilities/Route';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h2 className='fw-bold'>Quizi<span className='text-success'>.</span>fiy</h2>
-
+      <RouterProvider router={router} />
     </div>
   );
 }
---
+++ src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className='text-center'>This is home</h1>
+            <h2 className='fw-bold'>Quizi<span className='text-success'>.</span>fiy</h2>
+        </div>
+    );
+};
+
+export default Home;
