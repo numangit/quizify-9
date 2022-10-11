530d7182344a57f2fb55e09bb48450da38bb6e04
numangit <abdulnumanjob@gmail.com>
2022-10-11 10:57:08 +0600

    topic page added

--- src/components/Topic/Topic.js
+++ src/components/Topic/Topic.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { TopicsContext } from '../../layout/Main';
+import Header from '../Header/Header';
+import TopicCard from '../TopicCard/TopicCard';
+import Container from 'react-bootstrap/Container';
 
 const Topic = () => {
+    const topics = useContext(TopicsContext);
     return (
-        <div className='my-5 py-5'>
-            <h2>This is Topic</h2>
+        <div className='pb-sm-5 py-5 mt-5 mt-md-0'>
+            <Container className="mt-3 mt-md-5">
+                <h3 className='fw-semibold fs-3 my-5 '> Available topics to practice : </h3>
+                {
+                    topics.data.map(topic => <TopicCard key={topic.id} topic={topic}></TopicCard>)
+                }
+            </Container >
         </div>
     );
 };
