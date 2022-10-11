2ff71990635949ea82a3cd39428a04f47943d58b
numangit <abdulnumanjob@gmail.com>
2022-10-11 21:01:55 +0600

    quiz radio button fixed and eye icon toats added

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -2,10 +2,13 @@ import React from 'react';
 import './Quiz.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye } from '@fortawesome/free-solid-svg-icons';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Quiz = (props) => {
     console.log(props.props);
-    const { question, id, correctAnswer, options } = props.props;
+    const { question, correctAnswer, options } = props.props;
+    const notify = () => toast.info(correctAnswer, { position: "top-center", theme: "dark" });
 
 
     return (
@@ -13,25 +16,29 @@ const Quiz = (props) => {
 
             <div className="d-flex justify-space-between mx-auto bg-quiz p-2 rounded col-12">
                 <p className="py-0 my-0 fs-5 fw-semibold m-1">{question}</p>
-                <p className="py-0 my-0 ms-auto text-end eye-icon"><small><FontAwesomeIcon icon={faEye} /></small></p>
+                <p onClick={notify} className="py-0 my-0 ms-auto text-end eye-icon"><small><FontAwesomeIcon icon={faEye} /></small></p>
+                <ToastContainer />
             </div>
             <div>
-                <div class="bg-dark p-2 text-start mt-2 btn-answer rounded">
-                    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" />
-                    <label class="form-check-label ms-2" for="flexRadioDefault1">{options[0]}</label>
-                </div>
-                <div class="bg-dark p-2 text-start mt-2 btn-answer rounded">
-                    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked />
-                    <label class="form-check-label ms-2" for="flexRadioDefault2">{options[1]}</label>
-                </div>
-                <div class="bg-dark p-2 text-start mt-2 btn-answer rounded">
-                    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked />
-                    <label class="form-check-label ms-2" for="flexRadioDefault2">{options[2]}</label>
-                </div>
-                <div class="bg-dark p-2 text-start mt-2 btn-answer rounded">
-                    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked />
-                    <label class="form-check-label ms-2" for="flexRadioDefault2">{options[3]}</label>
-                </div>
+                <label class="w-100 mt-2 p-2 form-check-label bg-dark text-start btn-answer rounded" for="flexRadioDefault1">
+                    <input class="form-check-input me-2" type="radio" name="flexRadioDefault" id="flexRadioDefault1" />
+                    {options[0]}
+                </label>
+
+                <label class="w-100 mt-2 p-2 form-check-label bg-dark text-start btn-answer rounded" for="flexRadioDefault2">
+                    <input class="form-check-input me-2" type="radio" name="flexRadioDefault" id="flexRadioDefault2" />
+                    {options[1]}
+                </label>
+
+                <label class="w-100 mt-2 p-2 form-check-label bg-dark text-start btn-answer rounded" for="flexRadioDefault3">
+                    <input class="form-check-input me-2" type="radio" name="flexRadioDefault" id="flexRadioDefault3" />
+                    {options[2]}
+                </label>
+
+                <label class="w-100 mt-2 p-2 form-check-label bg-dark text-start btn-answer rounded" for="flexRadioDefault4">
+                    <input class="form-check-input me-2" type="radio" name="flexRadioDefault" id="flexRadioDefault4" />
+                    {options[3]}
+                </label>
             </div>
         </div>
     );
