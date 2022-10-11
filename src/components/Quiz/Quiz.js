73d3101f9a135ad79172262290fa968e8e4c9ac7
numangit <abdulnumanjob@gmail.com>
2022-10-11 22:14:28 +0600

    result toast added

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -7,12 +7,22 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const Quiz = (props) => {
     console.log(props.props);
-    const { question, correctAnswer, options } = props.props;
-    const notify = () => toast.info(correctAnswer, { position: "top-center", theme: "dark" });
+    const { question, id, correctAnswer, options } = props.props;
+    const notify = () => toast.info("Answer : " + correctAnswer, { position: "top-center", theme: "dark" });
 
+    // answer notification toast function
+    const notifyResult = (answer) => {
+        if (answer === correctAnswer) {
+            return toast.success('Good job, thats a correct answer!',
+                { position: "top-center", theme: "dark" })
+        } else {
+            return toast.error(`Oops! wrong answer. Correct answer is ${correctAnswer} `,
+                { position: "top-center", theme: "dark" })
+        }
+    };
 
     return (
-        <div className='py-2 py-md-4 my-2 my-md-4 col-10 col-md-8 col-lg-7 mx-auto'>
+        <div className='py- py-md-3 my-2 my-md-3 col-10 col-md-8 col-lg-7 mx-auto'>
 
             <div className="d-flex justify-space-between mx-auto bg-quiz p-2 rounded col-12">
                 <p className="py-0 my-0 fs-5 fw-semibold m-1">{question}</p>
@@ -20,23 +30,23 @@ const Quiz = (props) => {
                 <ToastContainer />
             </div>
             <div>
-                <label class="w-100 mt-2 p-2 form-check-label bg-dark text-start btn-answer rounded" for="flexRadioDefault1">
-                    <input class="form-check-input me-2" type="radio" name="flexRadioDefault" id="flexRadioDefault1" />
+                <label className="w-100 mt-2 p-2 form-check-label bg-dark text-start btn-answer rounded" for={`flexRadioDefault${id}1`}>
+                    <input onClick={() => notifyResult(options[0])} className="form-check-input me-2" type="radio" name="flexRadioDefault" id={`flexRadioDefault${id}1`} />
                     {options[0]}
                 </label>
 
-                <label class="w-100 mt-2 p-2 form-check-label bg-dark text-start btn-answer rounded" for="flexRadioDefault2">
-                    <input class="form-check-input me-2" type="radio" name="flexRadioDefault" id="flexRadioDefault2" />
+                <label className="w-100 mt-2 p-2 form-check-label bg-dark text-start btn-answer rounded" for={`flexRadioDefault${id}2`}>
+                    <input onClick={() => notifyResult(options[1])} className="form-check-input me-2" type="radio" name="flexRadioDefault" id={`flexRadioDefault${id}2`} />
                     {options[1]}
                 </label>
 
-                <label class="w-100 mt-2 p-2 form-check-label bg-dark text-start btn-answer rounded" for="flexRadioDefault3">
-                    <input class="form-check-input me-2" type="radio" name="flexRadioDefault" id="flexRadioDefault3" />
+                <label className="w-100 mt-2 p-2 form-check-label bg-dark text-start btn-answer rounded" for={`flexRadioDefault${id}3`}>
+                    <input onClick={() => notifyResult(options[2])} className="form-check-input me-2" type="radio" name="flexRadioDefault" id={`flexRadioDefault${id}3`} />
                     {options[2]}
                 </label>
 
-                <label class="w-100 mt-2 p-2 form-check-label bg-dark text-start btn-answer rounded" for="flexRadioDefault4">
-                    <input class="form-check-input me-2" type="radio" name="flexRadioDefault" id="flexRadioDefault4" />
+                <label className="w-100 mt-2 p-2 form-check-label bg-dark text-start btn-answer rounded" for={`flexRadioDefault${id}4`}>
+                    <input onClick={() => notifyResult(options[3])} className="form-check-input me-2" type="radio" name="flexRadioDefault" id={`flexRadioDefault${id}4`} />
                     {options[3]}
                 </label>
             </div>
--- src/components/QuizRoute/QuizRoute.js
+++ src/components/QuizRoute/QuizRoute.js
@@ -10,6 +10,7 @@ const QuizRoute = () => {
     return (
         <div className="py-5 mt-5">
             <h1 className="my-0 py-0 fw-bold display-5">Quiz Of  <span className="text-green">{quizs.data.name}</span></h1>
+            <p className='p-2 col-md-8 mx-auto fs-5'> Yay! its quiz time, make sure to answer all the questions correct. Goodluck!</p>
             {
                 quizs.data.questions.map(quiz => <Quiz key={quiz.id} props={quiz} ></Quiz>)
             }
