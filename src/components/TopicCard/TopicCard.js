491fe23dcc2d2209cf1082edb33c565a034d588d
numangit <abdulnumanjob@gmail.com>
2022-10-11 11:10:16 +0600

    dynamic link is created

--- src/components/TopicCard/TopicCard.js
+++ src/components/TopicCard/TopicCard.js
@@ -5,7 +5,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 const TopicCard = ({ topic }) => {
-    const { logo, name, total } = topic;
+    const { id, logo, name, total } = topic;
     return (
         //tip: use col instead of w to get responsive width
         <div className="d-flex my-4 col-lg-7 col-12 mx-auto  bg-card text-white p-2 rounded-3">
@@ -13,8 +13,8 @@ const TopicCard = ({ topic }) => {
             <div className='mx-auto'>
                 <p className="fs-3 my-1 py-0 fw-bold">{name}</p>
                 <p className="d-none d-sm-block my-1 py-0">Practice and test your {name} by answering our quiz. The more you practice the better you get! </p>
-                <p className=" my-2 py-0 "><small><span className="fw-semibold">Total Quiz : </span>{total}</small></p>
-                <Link className="my-2 py-0 " ><button className="btn btn-outline-dark btn-sm rounded-pill fw-bolder">Get Started  <FontAwesomeIcon icon={faArrowRight} /> </button></Link>
+                <p className=" my-2 py-0 " ><small><span className="fw-semibold">Total Quiz : </span>{total}</small></p>
+                <Link className="my-2 py-0" to={`quiz/${id}`} ><button className="btn btn-outline-dark btn-sm rounded-pill fw-bolder">Get Started  <FontAwesomeIcon icon={faArrowRight} /> </button></Link>
             </div>
         </div>
     );
--- src/utilities/Route.js
+++ src/utilities/Route.js
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom';
 import Blog from '../components/Blog/Blog';
 import ErrorPage from '../components/ErrorPage/ErrorPage';
 import Home from '../components/Home/Home';
+import Quiz from '../components/Quiz/Quiz';
 import Statistics from '../components/Statistics/Statistics';
 import Topic from '../components/Topic/Topic';
 import Main from '../layout/Main';
@@ -20,7 +21,12 @@ export const router = createBrowserRouter([
             { path: 'home', element: <Home></Home> },
             { path: 'Topic', element: <Topic></Topic> },
             { path: 'Statistics', element: <Statistics></Statistics> },
-            { path: 'blog', element: <Blog></Blog> }
+            { path: 'blog', element: <Blog></Blog> },
+            {
+                path: 'quiz/:id',
+                element: <Quiz></Quiz>,
+                loader: ({ params }) => fetch(`https://openapi.programming-hero.com/api/quiz/${params.id}`)
+            }
         ])
     }
 
