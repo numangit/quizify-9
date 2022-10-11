d668ecb9a0a8425a551d6e05019d55908e505249
numangit <abdulnumanjob@gmail.com>
2022-10-11 23:54:02 +0600

    fav icon update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -13,7 +13,7 @@ const Header = () => {
                 <h1 className='display-4 fw-bolder m-0'>Welcome to <span className="text-green">Quizify</span> !</h1>
                 <p className='fs-4 mt-3 mt-sm-4'>Welcome to the world’s largest quiz community. Play a quiz or create your own. Join a virtual or live event. Quizify yourself. </p>
 
-                <Link to={window.location == 'http://localhost:3000/home' ? "../blog" : "blog"}><button type="button" className="btn btn-outline-success btn-lg m-1 m-md-3">Learn More  <FontAwesomeIcon icon={faAngleDoubleRight} /></button></Link>
+                <Link to={window.location == 'http://localhost:3000' ? "blog" : "../blog"}><button type="button" className="btn btn-outline-success btn-lg m-1 m-md-3">Learn More  <FontAwesomeIcon icon={faAngleDoubleRight} /></button></Link>
             </div>
         </div>
     );
